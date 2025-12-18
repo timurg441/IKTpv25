2687597534c544c56afad6c412aaeeb201f4bd8e
timurg441 <1ame.xyz07@gmail.com>
2025-12-18 13:51:40 +0200

    .

--- Autor_Register/Autor_Register.py
+++ Autor_Register/Autor_Register.py
@@ -6,7 +6,7 @@ while True:
     print("Vali tegevus:")
     print("1 - Registreerimine")
     print("2 - Autoriseerimine")
-    print("3 - Paroolivahetus")
+    print("3 - Nime või parooli muutmine")
     print("4 - Paroolitaastamine")
     print("5 - Välju")
     valik = input("Sisesta valik (1-5): ")
@@ -15,7 +15,7 @@ while True:
     elif valik == '2':
         autoriseerimine(k, s)
     elif valik == '3':
-        paroolivahetus(k, s)
+        muutmine(k, s)
     elif valik == '4':
         paroolitaastamine(k, s)
     elif valik == '5':
