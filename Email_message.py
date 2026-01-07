78bdaccb0347d4f3d37edf83cfcaf7195388d55b
timurg441 <1ame.xyz07@gmail.com>
2026-01-07 20:09:00 +0200

    Add files via upload

---
+++ Email_message.py
@@ -0,0 +1,47 @@
+import smtplib, ssl
+from email.message import EmailMessage
+
+def saada_email(saaja_email):
+    teema = "Test kiri Pythonist"
+    saatja_email = "1ame.xyz07@gmail.com"
+    parool = input("Sisesta rakenduse parool: ")
+
+    smtp_server = "smtp.gmail.com"
+    port = 587
+    context = ssl.create_default_context()
+
+    msg = EmailMessage()
+    msg["Subject"] = teema
+    msg["From"] = saatja_email
+    msg["To"] = saaja_email
+
+    html_content = """\
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>HTML email</title>
+</head>
+<body>
+    <h1>Sending an HTML email from Python</h1>
+    <p>Hello there,</p>
+    <a href="https://github.com/timurg441/IKTpv25">
+        Here's a link to my GitHub!
+    </a>
+</body>
+</html>
+"""
+
+    msg.set_content(html_content, subtype="html")
+
+    try:
+        with smtplib.SMTP(smtp_server, port) as server:
+            server.starttls(context=context)
+            server.login(saatja_email, parool)
+            server.send_message(msg)
+        print("E-kiri saadetud!")
+    except Exception as e:
+        print(f"Midagi läks valesti... {e}")
+
+kellele = input("Sisesta saaja e-posti aadress: ")
+saada_email(kellele)
